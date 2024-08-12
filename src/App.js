1e3750a728112f8a512e0f32710af10f6554e3cf
Charinthorn <156071866+Charinthorn@users.noreply.github.com>
2024-08-12 16:15:00 +0700

    commit event
    commit event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Header,Content,Footer } from './func-components'
-import Calendar from './class-components'
+import Button from "./class-components";
+import {Calculator2} from "./calculator";
+import {EventData2} from "./event-data";
 
-export default function App(){
-  return(
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+//file1
+//function App(){
+//  return <Button/>
+//}
+//export default App
+
+//file2
+//export default function App(){
+// return <Calculator2/>
+//}
+
+//file3
+export default function App() {
+  return <EventData2/>
 }
