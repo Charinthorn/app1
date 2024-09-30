5e88feb66473579230ebca78c94d57298c0c8a9e
Charinthorn <156071866+Charinthorn@users.noreply.github.com>
2024-09-30 11:38:26 +0700

    commit form
    commit form

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header2 from './context-header2'
 import Content2 from './context-content2'
 import Router2 from './router2';
 import Router3 from './router3';
+import FormSelectFile from './form-select-file';
 
 //export default function App(){
 //  return(
@@ -30,7 +31,8 @@ export default function App(){
 //   return <EventData2/>
 //   return <RefsFunc/>
 //   return <MessageBox/>
-     return <Router3/>
+//   return <Router3/>
+     return <FormSelectFile/>  
 }
 
 // export default function App(){
